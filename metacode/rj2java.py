cf0c68052c1f790a62c76e2fd28febfba1360082
Benja Fallenstein <b.fallenstein@gmx.de>
2005-03-29 22:14:50 +0300

    Add features to the .rj preprocessor
    - Allow the main class in a file to be declared @realtime; this
      will create a RealtimeObject.Factory for the class, and add
      a private no-argument constructor (used by the factory).
    
    - In a main class declared @realtime, interpret declarations like
    
          public @constructor(Object o1, Object o2) {
    	  this.o1 = o1;
    	  this.o2 = o2;
          }
    
          public @constructor newBooHoo(int i) {
              this.setValue(i);
          }
    
      as declarations for static methods for getting instances
      of the class.
    
      (In the first case, the name 'newInstance' is implicit;
      this is the default name for static constructor methods.)
    
      For example, the above will (in a class Foo) be transformed to:
    
          public Foo newInstance(Object o1, Object o2) {
    	  Foo the_new_object = (Foo)FACTORY.object();
    	  the_new_object.o1 = o1;
              the_new_object.o2 = o2;
    	  return the_new_object;
          }
    
          public Foo newBooHoo(int i) {
    	  Foo the_new_object = (Foo)FACTORY.object();
    	  the_new_object.setValue(i);
    	  return the_new_object;
          }
    
    - When parsing balanced braces {}, ignore braces in strings :-)
    
    darcs-hash:20050329191450-2ee97-ab0bd358db41c9ec96be7c71702e564b0abf7489

--- metacode/rj2java.py
+++ metacode/rj2java.py
@@ -84,35 +84,140 @@ f.close()
 def parse_balanced_parens(str, start):
     o = str.find('{', start)
     c = str.find('}', start)
+    s = str.find('"', start)
     lc = str.find('//', start)
     mc = str.find('/*', start)
 
     if o < 0:  o = len(str)
     if c < 0:  c = len(str)
+    if s < 0:  s = len(str)
     if lc < 0: lc = len(str)
     if mc < 0: mc = len(str)
 
-    if o < c and o < lc and o < mc:
+    if o < c and o < s and o < lc and o < mc:
         in_parens = parse_balanced_parens(str, o+1)
         return parse_balanced_parens(str, in_parens)
-    elif c < lc and c < mc:
+    elif c < s and c < lc and c < mc:
         return c+1
+    elif s < lc and s < mc:
+        e = s+1
+        while True:
+            e = str.find('"', e)
+            assert e > 0
+
+            n = 0
+            while str[e-n-1] == '\\':
+                assert e-n-1 > 0
+                n = n + 1
+
+            if (n % 2) == 0: break
+            print 'did not break'
+
+        return parse_balanced_parens(str, e+1)
     elif lc < mc:
         e = str.find('\n', lc)
         return parse_balanced_parens(str, e)
     else:
         e = str.find('*/', mc+2)
-        return parse_balanced_parens(str, e)
+        try:
+            return parse_balanced_parens(str, e)
+        except:
+            print str[e:]
+            assert 0
 
 
-new_realtime = re.compile(r'new\s*@realtime(-i)?\s*([^ \r\n\t\f\v\(\)]*)\s*\(([^\(\)]*)\)\s*\{')
-variable = re.compile(r'\s*(\S*)\s*(\S*)\s*')
-
-replacements = []
 
 lastbrace = src.rfind('}')
 code = src[:lastbrace]
 epilog = src[lastbrace:]
+
+
+
+# first, check whether the whole class is declared @realtime
+
+rmodifiers = r'((?:public|protected|private|static)\s+)*'
+rparents = r'((?:\s+(?:extends\s+\S*|implements\s+(?:\S*,\s*)*\S*))*)'
+rclass_decl = (
+    r'%(rmodifiers)s@realtime\s+%(rmodifiers)sclass\s+(\S*)%(rparents)s\s*{'
+    % locals())
+
+#print rclass_decl
+
+match = re.compile(rclass_decl).search(src)
+if match:
+    modifiersA, modifiersB, classname, parents = match.groups()
+
+    modifiers = (modifiersA or '') + (modifiersB or '')
+
+    #print 'mod', modifiers
+    #print 'class', classname
+    #print 'parents', parents
+
+    # do we have an extends? (else, need to extend RealtimeObject)
+
+    has_extends = re.compile('extends\s+(\S*)').search(parents) != None
+    #print 'has extends', has_extends
+
+    if not has_extends:
+        parents = 'extends RealtimeObject ' + parents
+
+    # now, replace the class declaration
+
+    code = code[:match.start()] + modifiers + ' class ' + classname + ' ' + \
+           parents + ' { \n private ' + classname + '() {} ' + \
+           code[match.end():]
+
+    # the factory
+
+    epilog = ('''
+        private static Factory FACTORY = new Factory() {
+        protected Object create() {
+                return new %(classname)s();
+            }
+        };
+    ''' % locals()) + epilog
+
+
+    # now, need to find constructors...
+
+    rconstructors = re.compile('@constructor\s*(\S*)\s*\(([^)]*)\)\s*{')
+
+    while True:
+        match = rconstructors.search(code)
+        if match == None: break
+
+        name, params = match.groups()
+
+        if name.strip() == '': name = 'newInstance'
+
+        start = match.start()
+        end   = parse_balanced_parens(code, match.end())
+
+        body = code[match.end() : end-1]
+
+        body = re.sub('this', 'the_new_object', body)
+
+        repl = '''
+            static %(classname)s %(name)s(%(params)s) {
+                %(classname)s the_new_object = (%(classname)s)FACTORY.object();
+                %(body)s
+                return the_new_object;
+            }
+        ''' % locals()
+
+        code = code[:start] + repl + code[end:]
+
+    
+
+
+            
+
+# now, check for @realtime anonymous inner classes
+
+new_realtime = re.compile(r'new\s*@realtime(-i)?\s*([^ \r\n\t\f\v\(\)]*)\s*\(([^\(\)]*)\)\s*\{')
+variable = re.compile(r'\s*(@set\s+)?(\S*)\s*(\S*)\s*')
+
+replacements = []
 
 
 n = 1
@@ -130,8 +235,7 @@ while True:
 
     is_interface = match.group(1) != None
     interface = match.group(2)
-    params_spec = match.group(3)
-    parameters = params_spec.split(',')
+    parameters = match.group(3).split(',')
     body = code[match.end() : end-1]
 
     impl = '_%s_%s' % ('_'.join(interface.split('.')), n)
@@ -141,9 +245,13 @@ while True:
 
     if parameters == ['']: parameters = []
 
-    fields = ';'.join(parameters) + ';'
-    params = [variable.match(p).group(2) for p in parameters]
+    fields = ';'.join([p for p in parameters if not p.strip().startswith('@set')]) + ';'
 
+    params_spec = ', '.join([match.group(2) + ' ' + match.group(3)
+                             for match in
+                             [variable.match(p) for p in parameters]])
+
+    params = [variable.match(p).group(3) for p in parameters]
     params_list = ','.join(params)
     params_code = '\n'.join(['%s.%s = %s;' % (var,p,p) for p in params])
 
@@ -159,8 +267,8 @@ while True:
     
     for pstr in parameters:
         xm = variable.match(pstr)
-        xclazz = xm.group(1)
-        xvar = xm.group(2)
+        xclazz = xm.group(2)
+        xvar = xm.group(3)
         if xclazz in ('int', 'float', 'double', 'byte', 'short', 'char',
                       'long'): continue
 
